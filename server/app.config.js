f6f0bc37d11bb0df21d71300ac2b22d815d77855
Pavittar Singh <29155477+pavittarx@users.noreply.github.com>
2020-06-25 16:08:10 +0530

    added placeholder file

---
+++ server/app.config.js
@@ -0,0 +1,4 @@
+// It is a local development file to load development config vars 
+// This current version of the file is only meant to resolve `require()` "Module Not Found" error, while deployments
+module.exports = {
+}
