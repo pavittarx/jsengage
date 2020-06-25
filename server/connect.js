254193a3ad284b7d8dec90ee99971ecde581a6b1
pavittarx <pavittarx@users.noreply.github.com>
2020-06-25 15:02:14 +0530

    Initial Commit

---
+++ server/connect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("./../app.config");
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.MONGO_DATASOURCE || config.MONGO_DATASOURCE,
+  mongooseOptions
+);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Wohoo!! Connection Success.");
+}).on("error", (err) => {
+  console.log("Some error occured \n", err);
+});
