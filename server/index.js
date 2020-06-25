d0fde0227692ac825bcd70af3e5596fe7ebaba02
pavittarx <pavittarx@users.noreply.github.com>
2020-06-25 21:14:28 +0530

    enable cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+
+const path = require("path");
+const cors = require("cors");
 const compression = require("compression");
 
-app.use(compression()).use(express.urlencoded({ extended: true }));
+app
+  .use(compression())
+  .use(express.urlencoded({ extended: true }))
+  .use(cors());
 // Binds React App with Express Server
 app.use(express.static(path.join(__dirname, "..", "build")));
 
+// Router Configuration
 const authRouter = require("./routes/authentication");
 app.use("/api", authRouter);
 
