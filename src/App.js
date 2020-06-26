2606e63e22cdb82cca074ba0f7d9de031233c622
pavittarx <pavittarx@users.noreply.github.com>
2020-06-26 07:57:26 +0530

    components routing success

--- src/App.js
+++ src/App.js
@@ -1,77 +1,23 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { TextField, Button } from "@material-ui/core/";
-import { Alert } from "@material-ui/lab";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-async function handleSignup(params, err, msg) {
-  // Clears up previous error messages
-  err("");
-  msg("");
-
-  axios
-    .post("/api/signup", params)
-    .then((res) => {
-      console.log(res);
-      msg("Signup Success");
-    })
-    .catch((error) => {
-      console.log(error.response, error.message);
-      err(error.response.data);
-    });
-}
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 export default () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [err, setErr] = useState("");
-  const [message, setMessage] = useState("");
-
   return (
-    <>
-      <form>
-        {err && <Alert severity="error"> {err} </Alert>}
-        {message && <Alert severity="success"> {message} </Alert>}
-        <TextField
-          id="name"
-          label="Name"
-          variant="standard"
-          required
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <TextField
-          id="email"
-          label="Email"
-          variant="standard"
-          required
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <br />
-        <TextField
-          id="password"
-          label="Password"
-          variant="standard"
-          type="password"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          required
-        />
-        <br />
-        <Button
-          variant="outlined"
-          color="primary"
-          onClick={() =>
-            handleSignup({ name, email, password }, setErr, setMessage)
-          }
-        >
-          Submit
-        </Button>
-      </form>
-    </>
+    <Router>
+      <Link to="/signup">Signup</Link>
+      <Link to="/login">Login</Link>
+
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
