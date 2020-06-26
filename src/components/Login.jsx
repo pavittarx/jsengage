18fe90e2d6f660a022437852af39bbd9ec34245a
pavittarx <pavittarx@users.noreply.github.com>
2020-06-26 07:57:07 +0530

    Login &  Signup components ready

---
+++ src/components/Login.jsx
@@ -0,0 +1,67 @@
+// import Signup from "./components/Signup";
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core/";
+import { Alert } from "@material-ui/lab";
+import axios from "axios";
+
+function handleLogin(params, err, msg) {
+  // Clear previous error messages
+  err("");
+  msg("");
+
+  axios
+    .post("/api/login", params)
+    .then((res) => {
+      console.log("Response", res);
+      msg("Login Success!");
+    })
+    .catch((error) => {
+      console.log(error);
+      err(error.response.data);
+    });
+}
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [err, setErr] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <>
+      <form>
+        {err && <Alert severity="error"> {err} </Alert>}
+        {message && <Alert severity="success"> {message} </Alert>}
+        <TextField
+          id="email"
+          label="Email"
+          variant="standard"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <TextField
+          id="password"
+          label="Password"
+          variant="standard"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+        <br />
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => handleLogin({ email, password }, setErr, setMessage)}
+        >
+          Login{" "}
+        </Button>
+      </form>
+    </>
+  );
+};
---
+++ src/components/Signup.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core/";
+import { Alert } from "@material-ui/lab";
+import axios from "axios";
+
+async function handleSignup(params, err, msg) {
+  // Clears up previous error messages
+  err("");
+  msg("");
+
+  axios
+    .post("/api/signup", params)
+    .then((res) => {
+      console.log(res);
+      msg("Signup Success");
+    })
+    .catch((error) => {
+      console.log(error.response, error.message);
+      err(error.response.data);
+    });
+}
+
+export default () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [err, setErr] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <>
+      <form>
+        {err && <Alert severity="error"> {err} </Alert>}
+        {message && <Alert severity="success"> {message} </Alert>}
+        <TextField
+          id="name"
+          label="Name"
+          variant="standard"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <TextField
+          id="email"
+          label="Email"
+          variant="standard"
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <br />
+        <TextField
+          id="password"
+          label="Password"
+          variant="standard"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+        <br />
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() =>
+            handleSignup({ name, email, password }, setErr, setMessage)
+          }
+        >
+          Submit
+        </Button>
+      </form>
+    </>
+  );
+};
